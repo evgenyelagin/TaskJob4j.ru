87b85c4a6c8f4f0a25409be0caa53f1faba78125
evgenyelagin <anykeysky@gmail.com>
2020-11-27 15:28:54 +0300

    ver 2.0

--- src/ru/array/Merge.java
+++ src/ru/array/Merge.java
@@ -1,15 +1,22 @@
 package ru.array;
 
-public class Merge { // new array consist from 2 array
+public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        int z = 0;
-        for(int i = 0; i < left.length; i++) {
-            rsl[i] = left[i];
-            z++;
-        }
-        for(int j = 0; j < right.length; j++) {
-            rsl[z++] = right[j];
+        int ileft = 0;
+        int iright = 0;
+        for (int irsl = 0; irsl < rsl.length; ++irsl) {
+            if (ileft < left.length) {
+                if (iright < right.length) {
+                    if (left[ileft] < right[iright])
+                        rsl[irsl] = left[ileft++];
+                    else
+                        rsl[irsl] = right[iright++];
+                } else {
+                    rsl[irsl] = left[ileft++];
+                }
+            } else
+                rsl[irsl] = right[iright++];
         }
         return rsl;
     }
